30f68cd473b3ee1340c2973f35c1c12fc303a6b0
Grace David <gracecandy.david@gmail.com>
2024-03-09 22:20:11 +0530

    Tst

--- src/unittest/python/batch_tests.py
+++ src/unittest/python/batch_tests.py
@@ -19,20 +19,5 @@ class TestPerson(unittest.TestCase):
         person = Person()
         self.assertEqual(person.get_name(0), 'There is no such user')
 
-    @patch('builtins.print')
-    def test_main(self, mocked_print):
-        person = Person()
-        person.set_name('DevOps')
-        person.set_name('John')
-        person.get_name(0)
-        person.get_name(1)
-
-        self.assertTrue(mocked_print.called)
-        expected_calls = [
-            unittest.mock.call('User DevOps has been added with id ', 0),
-            unittest.mock.call('User associated with id 0 is ', 'DevOps')
-        ]
-        mocked_print.assert_has_calls(expected_calls)
-
 if __name__ == '__main__':
     unittest.main()
