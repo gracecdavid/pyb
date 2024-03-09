16c616e828ad939325373061bbe16f141d18b4ee
Grace David <gracecandy.david@gmail.com>
2024-03-09 20:02:13 +0530

    Update

--- src/main/python/batch.py
+++ src/main/python/batch.py
@@ -1,11 +1,33 @@
 class Person:
+    """
+    A class representing a person with a name.
+    """
+
     name = []
 
     def set_name(self, user_name):
+        """
+        Sets the name of the person and returns the assigned id.
+
+        Args:
+            user_name (str): The name of the person.
+
+        Returns:
+            int: The id assigned to the person.
+        """
         self.name.append(user_name)
         return len(self.name) - 1
 
     def get_name(self, user_id):
+        """
+        Retrieves the name of the person associated with the given id.
+
+        Args:
+            user_id (int): The id of the person.
+
+        Returns:
+            str: The name of the person, or 'There is no such user' if the id is invalid.
+        """
         if user_id >= len(self.name):
             return 'There is no such user'
         return self.name[user_id]
