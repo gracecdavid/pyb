b14cdebc8c17c88d86eb2411fed904e18521c4b4
Grace David <gracecandy.david@gmail.com>
2024-03-09 21:31:23 +0530

    Check

--- src/unittest/python/batch_tests.py
+++ src/unittest/python/batch_tests.py
@@ -1,4 +1,5 @@
 import unittest
+from unittest.mock import patch
 import sys
 sys.path.append("c:\\Users\\Grace\\PYB\\src\\main\\python")
 from batch import Person
@@ -17,6 +18,21 @@ class TestPerson(unittest.TestCase):
     def test_get_name_non_existing(self):
         person = Person()
         self.assertEqual(person.get_name(0), 'There is no such user')
+
+    @patch('builtins.print')
+    def test_main(self, mocked_print):
+        person = Person()
+        person.set_name('DevOps')
+        person.set_name('John')
+        person.get_name(0)
+        person.get_name(1)
+
+        self.assertTrue(mocked_print.called)
+        expected_calls = [
+            unittest.mock.call('User DevOps has been added with id ', 0),
+            unittest.mock.call('User associated with id 0 is ', 'DevOps')
+        ]
+        mocked_print.assert_has_calls(expected_calls)
 
 if __name__ == '__main__':
     unittest.main()
