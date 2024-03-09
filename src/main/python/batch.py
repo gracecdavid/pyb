b220b9d358ba7a5a98e8198dc07fa1df5a4b8508
Grace David <gracecandy.david@gmail.com>
2024-03-09 19:55:55 +0530

    Updated code

--- src/main/python/batch.py
+++ src/main/python/batch.py
@@ -8,8 +8,7 @@ class Person:
     def get_name(self, user_id):
         if user_id >= len(self.name):
             return 'There is no such user'
-        else:
-            return self.name[user_id]
+        return self.name[user_id]
 
 
 if __name__ == '__main__':
