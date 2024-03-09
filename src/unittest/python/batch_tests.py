24e943b3616357d781460399481568d4bed1c326
Grace David <gracecandy.david@gmail.com>
2024-03-09 20:30:12 +0530

    Finals

--- src/unittest/python/batch_tests.py
+++ src/unittest/python/batch_tests.py
@@ -1,23 +1,19 @@
 import unittest
 
-class Testing(unittest.TestCase):
-    def test_string_equal(self):
-        a = 'some'
-        b = 'some'
-        self.assertEqual(a, b)
+class TestPerson(unittest.TestCase):
+    def test_set_name(self):
+        person = Person()
+        self.assertEqual(person.set_name('DevOps'), 0)
+        self.assertEqual(person.set_name('John'), 1)
 
-    def test_string_not_equal(self):
-        a = 'some'
-        b = 'different'
-        self.assertNotEqual(a, b)
+    def test_get_name_existing(self):
+        person = Person()
+        person.set_name('DevOps')
+        self.assertEqual(person.get_name(0), 'DevOps')
 
-    def test_boolean_true(self):
-        a = True
-        self.assertTrue(a)
-
-    def test_boolean_false(self):
-        a = False
-        self.assertFalse(a)
+    def test_get_name_non_existing(self):
+        person = Person()
+        self.assertEqual(person.get_name(0), 'There is no such user')
 
 if __name__ == '__main__':
     unittest.main()
